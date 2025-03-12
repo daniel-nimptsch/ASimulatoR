a89e4e4d9850830f730b87e0bb0e2477851d99f3
Daniel Nimptsch <danielnimptsch@hotmail.de>
2025-03-12 12:47:33 +0100

    refactor: Uncomment simulation and parameter initialization in R script

--- R/simulate_alternative_splicing.R
+++ R/simulate_alternative_splicing.R
@@ -274,18 +274,18 @@ simulate_alternative_splicing <-
       }
     }))
 
-    # params$meanmodel <- T
-    # params$frag_GC_bias <- NULL
-    # params$gtf <- file.path(outdir, 'splicing_variants.gtf')
-    # params$seqpath <- input_dir
-    # params$outdir <- outdir
+    params$meanmodel <- T
+    params$frag_GC_bias <- NULL
+    params$gtf <- file.path(outdir, 'splicing_variants.gtf')
+    params$seqpath <- input_dir
+    params$outdir <- outdir
 
     ### simulate with polyester----
-    # message('start simulation with polyester:')
-    # do.call(simulate_experiment, params)
+    message('start simulation with polyester:')
+    do.call(simulate_experiment, params)
 
     ### Write additional output files ----
-    message('Saving exon junction table and fold changes to CSV files...')
+    # message('Saving exon junction table and fold changes to CSV files...')
     # Write exon junction table to CSV
     if (!is.null(params$exon_junction_table)) {
       ejt_file <- file.path(outdir, 'exon_junction_table.csv')
@@ -293,12 +293,12 @@ simulate_alternative_splicing <-
       message(sprintf('Exon junction table saved to %s', ejt_file))
     }
 
-    # Write fold changes to CSV
-    if (!is.null(params$fold_changes)) {
-      fc_file <- file.path(outdir, 'fold_changes.csv')
-      utils::write.csv(params$fold_changes, fc_file, row.names = FALSE)
-      message(sprintf('Fold changes saved to %s', fc_file))
-    }
+    # # Write fold changes to CSV
+    # if (!is.null(params$fold_changes)) {
+    #   fc_file <- file.path(outdir, 'fold_changes.csv')
+    #   utils::write.csv(params$fold_changes, fc_file, row.names = FALSE)
+    #   message(sprintf('Fold changes saved to %s', fc_file))
+    # }
 
     # Restore whatever RNG the user had set before running this function
     RNGkind(old_rng[1], old_rng[2])
